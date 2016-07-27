7ba4bd280ebdbe53a3ee374087b758b0b6c72dc6
perl <perl@jzone.me>
2016-07-27 19:51:04 +0800

    优化touch_and_basic.js的性能

--- touch_and_basic.js
+++ touch_and_basic.js
@@ -62,27 +62,27 @@ sys.te = function() {
 }
 //手指往下滑，切换场景
 sys.movedown = function(){
+	$("#section_"+ahk.step).j_transition({timingFunc:"cubic-bezier(0.04, 1.01, 0.18, 0.96)",duration:"0.4s"});
 	if(ahk.step > 0 && Math.abs(sys.startY - sys.endY) > 20){
-		var tp = ahk.step;
-		$("#section_"+tp).j_transition({timingFunc:"cubic-bezier(0.04, 1.01, 0.18, 0.96)",duration:"0.4s"});
-		$("#section_"+tp).j_transform({y:0})
+		$("#section_"+ahk.step).j_transform({y:0})
 		ahk.step--;
 		ahk.scene();
 	}else{
-		$("#section_"+ahk.step).j_transition({timingFunc:"cubic-bezier(0.04, 1.01, 0.18, 0.96)",duration:"0.4s"});
 		$("#section_"+ahk.step).j_transform({y:(-ahk.h * ahk.step)+"px"})
 	}
 }
 //手指往上滑，切换场景
 sys.moveup = function(){
+	var a = function()}{
+		$("#section_"+ahk.step).j_transition({timingFunc:"cubic-bezier(0.04, 1.01, 0.18, 0.96)",duration:"0.4s"});
+		$("#section_"+ahk.step).j_transform({y:(-ahk.h * ahk.step)+"px"})
+	}
 	if(ahk.step < ahk.size -1 && Math.abs(sys.startY - sys.endY) > 20){
 		ahk.step++;
-		$("#section_"+ahk.step).j_transition({timingFunc:"cubic-bezier(0.04, 1.01, 0.18, 0.96)",duration:"0.4s"});
-		$("#section_"+ahk.step).j_transform({y:(-ahk.h * ahk.step)+"px"})
+		a();
 		ahk.scene();
 	}else{
-		$("#section_"+ahk.step).j_transition({timingFunc:"cubic-bezier(0.04, 1.01, 0.18, 0.96)",duration:"0.4s"});
-		$("#section_"+ahk.step).j_transform({y:(-ahk.h * ahk.step)+"px"})
+		a();
 	}
 }
 //播放和暂停音乐
